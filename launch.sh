5c9f2f455f55988917310e80383030e2d993dafd
Charles Brandt <code@charlesbrandt.com>
2015-12-19 07:58:49 -0500

    updates to process notes

--- launch.sh
+++ launch.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # launch.py is availble in:
-# https://github.com/charlesbrandt/mindstream
+# https://github.com/charlesbrandt/moments
 
 # path to launch.py is defined in:
 # ~/.bashrc
-# example .bashrc is available in:
-# https://github.com/charlesbrandt/moments
-# in editors directory
+# example .bashrc is available in moments/editors/ directory
 
-python /c/mindstream/mindstream/launch.py -c /c/public/system system
+launch.py -c /c/public/system system
 
 echo "see also:
 
-python /c/mindstream/mindstream/launch.py -c /c/public/system macosx
+launch.py -c /c/public/system system
 
-python /c/mindstream/mindstream/launch.py -c /c/public/system ansible
+launch.py -c /c/public/system macosx
+
+launch.py -c /c/public/system ansible
 
 ansible-playbook system.yml -i hosts.txt --ask-become-pass
 "
