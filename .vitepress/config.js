436207be17915c6d5f27e637fbca1c0c74c030db
Charles Brandt <2759041-charlesbrandt@users.noreply.gitlab.com>
2021-09-25 16:56:06 -0400

    add in vitepress config

---
+++ .vitepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  title: 'Notes',
+  description: 'Still learning. Topics of interest. Writing, Code Development, Computer Systems, The Web, Design',
+  themeConfig: {
+    nav: [
+      { text: 'Code', link: '/code/' },
+      { text: 'System', link: '/system/' },
+      { text: 'Design', link: '/design/' },
+      { text: 'Documentation', link: '/documentation/' },
+      { text: 'Pi', link: '/pi/' },
+      { text: 'Web', link: '/web/' },
+    //  { text: '', link: '/' },
+
+    ],
+    //sidebar: [
+    //  { text: 'Sidebar Test ', link: '/' },
+    //  { text: '', link: '/' },
+    //]
+  }
+}
---
+++ .vitepress/theme/index.js
@@ -0,0 +1,37 @@
+// https://vitepress.vuejs.org/guide/theming.html#using-a-custom-theme
+
+// interface Theme {
+//   Layout: Component // Vue 3 component
+//   NotFound?: Component
+//   enhanceApp?: (ctx: EnhanceAppContext) => void
+// }
+
+// interface EnhanceAppContext {
+//   app: App // Vue 3 app instance
+//   router: Router // VitePress router instance
+//   siteData: Ref<SiteData>
+// }
+
+import Layout from './layout.vue'
+
+import DefaultTheme from 'vitepress/theme'
+
+export default {
+  ...DefaultTheme,
+  enhanceApp({ app }) {
+    // register global components
+    app.component('MyGlobalComponent' /* ... */)
+  }
+}
+
+/*
+export default {
+  Layout,
+  NotFound: () => 'custom 404', // <- this is a Vue 3 functional component
+  enhanceApp({ app, router, siteData }) {
+    // app is the Vue 3 app instance from `createApp()`.
+    // router is VitePress' custom router.
+    `siteData`` is a `ref`` of current site-level metadata.
+  }
+}
+*/
